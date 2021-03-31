7394103a97057377c061a121fc88a48265a69559
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2021-03-31 18:41:57 -0400

    Update DemoClass.java
    conflicting collaborator commit

--- src/package01/DemoClass.java
+++ src/package01/DemoClass.java
@@ -4,6 +4,8 @@ public class DemoClass {
 
     //Hello everyone
     //Hello Guys! Did you just join?
+    //Here are some updates from remote collaborators
+    //Here are the second line of conflict
 
 
 }
